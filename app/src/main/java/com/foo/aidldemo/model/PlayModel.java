f51f63ea99c3ca5040bee685803b12b60f0342d5
meijieman <1558667079@qq.com>
2016-08-18 20:21:54 +0800

    a

--- app/src/main/java/com/foo/aidldemo/model/PlayModel.java
+++ app/src/main/java/com/foo/aidldemo/model/PlayModel.java
@@ -21,7 +21,7 @@ public class PlayModel {
                 SystemClock.sleep(3000);
                 List<Music> data = new ArrayList<>();
                 Music music = new Music();
-                music.name = "度红尘";
+                music.name = "渡红尘";
                 music.id = 1001;
                 music.imgUrl = "http://img.xiami.net/images/album/img96/683379096/21003568111466524385_1.jpg";
                 music.url = "http://m5.file.xiami.com/201/1506278201/2100356811/1776200826_60350905_l.mp3?auth_key=5f8e6a87570d5bc6c1c88eeda8d8c4cb-1477065600-0-null";
